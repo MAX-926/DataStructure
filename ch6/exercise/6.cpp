43f64a829da1511d1859314d0d1e5ed89e87c9d6
Serverus <1913125@mail.nankai.edu.cn>
2024-05-25 11:07:20 +0800

    ipdate

---
+++ ch6/exercise/6.cpp
@@ -0,0 +1,32 @@
+#include"../case/6-2.h"
+
+template<class T>
+class chainListWithOperatorLocate:public chain<T>
+{
+    public:
+        chainListWithOperatorLocate(int initialCapacity = 10):chain<T>(initialCapacity){}
+        T& operator[](int theIndex);
+};
+
+template<class T>
+T& chainListWithOperatorLocate<T>::operator[](int theIndex)
+{
+    this->checkIndex(theIndex);
+    chainNode<T>* current = (this->firstNode);
+    for(int i = 0; i < theIndex; i++)
+        current = current->next;
+    return current->element;
+}
+
+int main()
+{
+    chainListWithOperatorLocate<int> x;
+    for(int i = 0; i < 5; i++)
+        x.insert(i,i+1);
+    cout << x << endl;
+    try
+    {
+    x[0] = 26;
+    cout << x[-1] << endl;
+    }catch(IllegalParameter ip){ip.outputMessage();}
+}
